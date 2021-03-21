c7474b48f96e40082b70df402c2249d5aa065015
TAESUN YOO <ytshaha@naver.com>
2021-03-21 15:03:39 +0900

    210321_email iuoi

--- mysite/Google.py
+++ mysite/Google.py
@@ -274,3 +274,75 @@ def Create_Service(client_secret_file, api_name, api_version, *scopes):
 def convert_to_RFC_datetime(year=1900, month=1, day=1, hour=0, minute=0):
     dt = datetime.datetime(year, month, day, hour, minute, 0).isoformat() + 'Z'
     return dt
+
+
+
+
+##########################
+# 유태선이 다시 만든다.
+
+
+import pickle
+import datetime
+import os
+from google_auth_oauthlib.flow import Flow, InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
+from google.auth.transport.requests import Request
+
+
+import logging
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+from apiclient.discovery import build
+
+from django.conf import settings
+# ...
+
+def Create_Service2(client_secret_file, api_name, api_version, *scopes):
+    print(client_secret_file, api_name, api_version, scopes, sep='-')
+    CLIENT_SECRET_FILE = client_secret_file
+    API_SERVICE_NAME = api_name
+    API_VERSION = api_version
+    SCOPES = [scope for scope in scopes[0]]
+    print(SCOPES)
+    
+    cred = None
+
+    pickle_file = f'token_{API_SERVICE_NAME}_{API_VERSION}.pickle'
+    # print(pickle_file)
+    
+    if os.path.exists(pickle_file):
+        with open(pickle_file, 'rb') as token:
+            cred = pickle.load(token)
+
+    if not cred or not cred.valid:
+        if cred and cred.expired and cred.refresh_token:
+            cred.refresh(Request())
+        else:
+            # flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
+            flow = flow_from_clientsecrets(CLIENTSECRETS_LOCATION, ' '.join(SCOPES))
+            flow.redirect_uri = REDIRECT_URI
+            # try:
+            #     credentials = flow.step2_exchange(authorization_code)
+            # except FlowExchangeError:
+            #     logging.error('An error occurred: %s', error)
+            #     raise CodeExchangeException(None)
+            # cred = flow.run_console()
+            cred = flow.run_local_server(host='REDIRECT_URI')
+
+        with open(pickle_file, 'wb') as token:
+            pickle.dump(credentials, token)
+
+    try:
+        service = build(API_SERVICE_NAME, API_VERSION, credentials=credentials)
+        print(API_SERVICE_NAME, 'service created successfully')
+        return service
+    except Exception as e:
+        print(e)
+        return None
+
+
+def convert_to_RFC_datetime(year=1900, month=1, day=1, hour=0, minute=0):
+    dt = datetime.datetime(year, month, day, hour, minute, 0).isoformat() + 'Z'
+    return dt
