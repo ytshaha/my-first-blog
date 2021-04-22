335e051902d950001a2a31ae4a43ce3dbdca89a5
Taesun Yoo <ytshaha@naver.com>
2021-04-22 22:27:07 +0900

    210422_update templates.. mainly product_list

--- products/views.py
+++ products/views.py
@@ -40,7 +40,7 @@ class ProductNormalListView(LoginRequiredMixin, generic.ListView):
     '''
     template_name = 'products/product_list.html'
     context_object_name = 'product_items'
-    paginate_by = 3
+    paginate_by = 20
 
     def get_context_data(self, *args, **kwargs):
         context = super(ProductNormalListView, self).get_context_data(*args, **kwargs)
--- search/views.py
+++ search/views.py
@@ -1,8 +1,16 @@
 
 from django.shortcuts import render
 from django.views import generic
-from products.models import ProductItem
-
+from products.models import ProductItem, Brand, Category
+from wishlist.models import Wish
+CATEGORY_CHOICES = (
+    ('woman', '여성의류'),
+    ('man', '남성의류'),
+    ('shoes', '신발'),
+    ('bag', '가방'),
+    ('acc', '명품잡화'),
+    ('kid', '키즈'),
+)
 class SearchProductListView(generic.ListView):
     template_name = 'search/view.html'
     context_object_name = 'product_items'
@@ -11,7 +19,16 @@ class SearchProductListView(generic.ListView):
         context = super(SearchProductListView, self).get_context_data(*args, **kwargs)
         query = self.request.GET.get('q')
         context['query'] = query
+        brands = Brand.objects.all()
         # SearchQuery.objects.create(query=query)
+        context['brands'] = brands
+        context['is_staff_check'] = False
+        
+        user = self.request.user
+        wish_list = Wish.objects.filter(user=user).values_list('product_item', flat=True)
+        context['wish_list'] = wish_list
+
+        context['category_qs'] = CATEGORY_CHOICES
         return context
 
     def get_queryset(self, *args, **kwargs):
